9c68a4d52ef2147dc35f7dd73088a12b32f7d57f
Rotem Tamir <rotemtamir@gmail.com>
2023-09-23 15:39:07 +0300

    all

--- atlasaction/action.go
+++ atlasaction/action.go
@@ -1,146 +1,36 @@
 package atlasaction
 
 import (
-	"bytes"
 	"context"
 	_ "embed"
 	"fmt"
 	"strconv"
-	"strings"
-	"text/template"
 
 	"ariga.io/atlas-go-sdk/atlasexec"
 	"github.com/sethvargo/go-githubactions"
 )
 
-func Version(ctx context.Context, client *atlasexec.Client, act *githubactions.Action) error {
-	if input := act.GetInput("test-input"); input != "" {
-		act.Infof("test-input: %s", input)
-	}
-	version, err := client.Version(ctx)
-	if err != nil {
-		return err
-	}
-	act.SetOutput("version", version.Version)
-	return nil
-}
-
-var (
-	//go:embed atlas.hcl.tmpl
-	tmpl   string
-	config = template.Must(template.New("atlashcl").Parse(tmpl))
-)
-
 // MigrateApply runs the GitHub Action for "ariga/atlas-action/migrate/apply".
 func MigrateApply(ctx context.Context, client *atlasexec.Client, act *githubactions.Action) error {
-	input, err := migApplyInput(act)
+	params := &atlasexec.MigrateApplyParams{
+		URL:             act.GetInput("url"),
+		DirURL:          act.GetInput("dir"),
+		TxMode:          act.GetInput("tx-mode"),  // Hidden param.
+		BaselineVersion: act.GetInput("baseline"), // Hidden param.
+	}
+	run, err := client.MigrateApply(ctx, params)
 	if err != nil {
+		act.SetOutput("error", err.Error())
 		return err
 	}
-	run, err := migrateApply(ctx, client, input)
-	if err != nil {
-		return err
+	if run.Error != "" {
+		act.SetOutput("error", run.Error)
+		return fmt.Errorf("run failed: %s", run.Error)
 	}
-	act.SetOutput("error", run.Error)
 	act.SetOutput("current", run.Current)
 	act.SetOutput("target", run.Target)
 	act.SetOutput("pending_count", strconv.Itoa(len(run.Pending)))
 	act.SetOutput("applied_count", strconv.Itoa(len(run.Applied)))
 	act.Infof("Run complete: +%v", run)
 	return nil
-}
-
-type (
-	// migrateApplyInput is created from the GitHub Action "with" configuration.
-	migrateApplyInput struct {
-		// URL is the target database URL.
-		URL string
-		// Amount is the number of migrations to apply.
-		Amount uint64
-		// TxMode is the Atlas transaction mode to use.
-		TxMode string
-		// Baseline is the baseline version to use.
-		Baseline string
-		// AllowDirty instructs Atlas whether to allow a dirty target database.
-		AllowDirty bool
-		// Dir is the directory to use for migrations. Atlas expects this to be a URL
-		// such as file://path/to/migrations.
-		Dir string
-		// RevisionsSchema is the schema to use for the revisions table.
-		RevisionsSchema string
-
-		// Cloud is the Atlas Cloud configuration.
-		Cloud cloud
-	}
-	cloud struct {
-		// The target directory slug.
-		Dir string
-		// The tag for the migration directory revision to use.
-		Tag string
-		// The Atlas Cloud endpoint URL.
-		URL string
-	}
-)
-
-// migApplyInput loads the input from the GitHub Action configuration.
-func migApplyInput(act *githubactions.Action) (*migrateApplyInput, error) {
-	i := &migrateApplyInput{
-		URL: act.GetInput("url"),
-	}
-	if i.URL == "" {
-		return nil, fmt.Errorf("url is required")
-	}
-	if txm := act.GetInput("tx-mode"); txm != "" {
-		switch txm {
-		case "all", "none", "file":
-			i.TxMode = txm
-		default:
-			return nil, fmt.Errorf("invalid tx-mode %q", txm)
-		}
-		i.TxMode = act.GetInput("tx-mode")
-	}
-	i.Baseline = act.GetInput("baseline")
-	if ad := act.GetInput("allow-dirty"); ad != "" {
-		allowDirty, err := strconv.ParseBool(strings.ToLower(ad))
-		if err != nil {
-			return nil, fmt.Errorf("invalid allow-dirty %q", ad)
-		}
-		i.AllowDirty = allowDirty
-	}
-	i.Dir = act.GetInput("dir")
-	i.Cloud.Dir = act.GetInput("dir-name")
-	if i.Dir != "" && i.Cloud.Dir != "" {
-		return nil, fmt.Errorf("dir and dir-name are mutually exclusive")
-	}
-	i.Cloud.URL = act.GetInput("cloud-url")
-	i.Cloud.Tag = act.GetInput("tag")
-	return i, nil
-}
-
-// migrateApply runs the "migrate apply" for the input.
-func migrateApply(ctx context.Context, client *atlasexec.Client, i *migrateApplyInput) (*atlasexec.ApplyReport, error) {
-	params := &atlasexec.MigrateApplyParams{
-		URL:             i.URL,
-		Amount:          i.Amount,
-		TxMode:          i.TxMode,
-		BaselineVersion: i.Baseline,
-	}
-	if i.Dir != "" {
-		params.DirURL = i.Dir // We let Atlas verify that its an actual URL.
-	}
-	if i.Cloud.Dir != "" {
-		var buf bytes.Buffer
-		if err := config.Execute(&buf, i); err != nil {
-			return nil, err
-		}
-		cfg, clean, err := atlasexec.TempFile(buf.String(), "hcl")
-		if err != nil {
-			return nil, err
-		}
-		// nolint:errcheck
-		defer clean()
-		params.ConfigURL = cfg
-		params.Env = "atlas"
-	}
-	return client.MigrateApply(ctx, params)
 }
--- atlasaction/action_test.go
+++ atlasaction/action_test.go
@@ -4,80 +4,65 @@ import (
 	"bytes"
 	"context"
 	"database/sql"
-	"encoding/base64"
 	"fmt"
-	"net/http"
-	"net/http/httptest"
 	"os"
 	"path/filepath"
 	"strings"
 	"testing"
 
 	"ariga.io/atlas-go-sdk/atlasexec"
-	"ariga.io/atlas/sql/migrate"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/sethvargo/go-githubactions"
 	"github.com/stretchr/testify/require"
 )
 
 func TestMigrateApply(t *testing.T) {
-	file, err := os.CreateTemp("", "")
-	require.NoError(t, err)
-	db := sqlitedb(t)
-	env := map[string]string{
-		"INPUT_DIR":     "file://testdata/migrations/",
-		"INPUT_URL":     "sqlite://" + db,
-		"GITHUB_OUTPUT": file.Name(),
-	}
+	t.Run("local dir", func(t *testing.T) {
+		tt := newT(t)
+		tt.setInput("url", "sqlite://"+tt.db)
+		tt.setInput("dir", "file://testdata/migrations/")
 
-	var b bytes.Buffer
-	act := githubactions.New(
-		githubactions.WithGetenv(func(key string) string {
-			return env[key]
-		}),
-		githubactions.WithWriter(&b),
-	)
-	cli, err := atlasexec.NewClient("", "atlas")
-	require.NoError(t, err)
-	err = MigrateApply(context.Background(), cli, act)
-	require.NoError(t, err)
-	readFile, err := os.ReadFile(file.Name())
-	require.NoError(t, err)
-	m, err := readOutputFile(string(readFile))
-	require.NoError(t, err)
-	require.EqualValues(t, map[string]string{
-		"error":         "",
-		"current":       "",
-		"target":        "20230922132634",
-		"pending_count": "1", // Pending count before we started running.
-		"applied_count": "1",
-	}, m)
-}
-
-// fakeCloud returns a httptest.Server that mocks the cloud endpoint.
-func fakeCloud(t *testing.T) *httptest.Server {
-	dir := testDir(t, "./internal/testdata/migrations")
-	ad, err := migrate.ArchiveDir(&dir)
-	require.NoError(t, err)
-	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		require.Equal(t, "Bearer token", r.Header.Get("Authorization"))
-		// nolint:errcheck
-		fmt.Fprintf(w, `{"data":{"dir":{"content":%q}}}`, base64.StdEncoding.EncodeToString(ad))
-	}))
-	return srv
-}
-
-// testDir returns a migrate.MemDir from the given path.
-func testDir(t *testing.T, path string) (d migrate.MemDir) {
-	rd, err := os.ReadDir(path)
-	require.NoError(t, err)
-	for _, f := range rd {
-		fp := filepath.Join(path, f.Name())
-		b, err := os.ReadFile(fp)
+		err := MigrateApply(context.Background(), tt.cli, tt.act)
 		require.NoError(t, err)
-		require.NoError(t, d.WriteFile(f.Name(), b))
-	}
-	return d
+
+		m, err := tt.outputs()
+		require.EqualValues(t, map[string]string{
+			"error": "",
+		}, m)
+	})
+	t.Run("tx-mode", func(t *testing.T) {
+		tt := newT(t)
+		tt.setInput("url", "sqlite://"+tt.db)
+		tt.setInput("dir", "file://testdata/migrations/")
+		tt.setInput("tx-mode", "fake")
+		err := MigrateApply(context.Background(), tt.cli, tt.act)
+
+		// An error here proves that the tx-mode was passed to atlasexec, which
+		// is what we want to test.
+		exp := `run failed: unknown tx-mode "fake"`
+		require.EqualError(t, err, exp)
+		m, err := tt.outputs()
+		require.EqualValues(t, map[string]string{
+			"error": exp,
+		}, m)
+	})
+	t.Run("baseline", func(t *testing.T) {
+		tt := newT(t)
+		tt.setInput("url", "sqlite://"+tt.db)
+		tt.setInput("dir", "file://testdata/migrations/")
+		tt.setInput("baseline", "111_fake")
+		err := MigrateApply(context.Background(), tt.cli, tt.act)
+
+		// An error here proves that the baseline was passed to atlasexec, which
+		// is what we want to test.
+		exp := `atlasexec: baseline version "111_fake" not found`
+		require.EqualError(t, err, exp)
+		m, err := tt.outputs()
+		require.EqualValues(t, map[string]string{
+			"error": exp,
+		}, m)
+	})
+
 }
 
 // sqlitedb returns a path to an initialized sqlite database file. The file is
@@ -90,41 +75,53 @@ func sqlitedb(t *testing.T) string {
 	return dbpath
 }
 
-func TestParseGitHubOutputFile(t *testing.T) {
-	tmp, err := os.CreateTemp("", "")
-	require.NoError(t, err)
-	defer os.Remove(tmp.Name())
-	act := githubactions.New(
-		githubactions.WithGetenv(func(key string) string {
-			return map[string]string{
-				"GITHUB_OUTPUT": tmp.Name(),
-			}[key]
-		}),
-	)
-	act.SetOutput("foo", "bar")
-	act.SetOutput("baz", "qux")
-	file, err := os.ReadFile(tmp.Name())
-	require.NoError(t, err)
-	m, err := readOutputFile(string(file))
-	require.NoError(t, err)
-	require.EqualValues(t, map[string]string{
-		"foo": "bar",
-		"baz": "qux",
-	}, m)
+type test struct {
+	db  string
+	env map[string]string
+	out bytes.Buffer
+	cli *atlasexec.Client
+	act *githubactions.Action
 }
 
-// readOutputFile is a test helper that parses the GitHub Actions output file format. This is
+func newT(t *testing.T) *test {
+	outputFile, err := os.CreateTemp("", "")
+	require.NoError(t, err)
+	tt := &test{
+		db: sqlitedb(t),
+		env: map[string]string{
+			"GITHUB_OUTPUT": outputFile.Name(),
+		},
+	}
+	tt.act = githubactions.New(
+		githubactions.WithGetenv(func(key string) string {
+			return tt.env[key]
+		}),
+		githubactions.WithWriter(&tt.out),
+	)
+	cli, err := atlasexec.NewClient("", "atlas")
+	require.NoError(t, err)
+	tt.cli = cli
+	return tt
+}
+
+func (t *test) setInput(k, v string) {
+	t.env["INPUT_"+strings.ToUpper(k)] = v
+}
+
+// outputs is a helper that parses the GitHub Actions output file format. This is
 // used to parse the output file written by the action.
-func readOutputFile(content string) (map[string]string, error) {
-	const token = "_GitHubActionsFileCommandDelimeter_"
-
-	m := make(map[string]string)
-	lines := strings.Split(content, "\n")
-
+func (t *test) outputs() (map[string]string, error) {
 	var (
 		key   string
 		value strings.Builder
+		token = "_GitHubActionsFileCommandDelimeter_"
 	)
+	m := make(map[string]string)
+	c, err := os.ReadFile(t.env["GITHUB_OUTPUT"])
+	if err != nil {
+		return nil, err
+	}
+	lines := strings.Split(string(c), "\n")
 	for _, line := range lines {
 		if delim := "<<" + token; strings.Contains(line, delim) {
 			key = strings.TrimSpace(strings.Split(line, delim)[0])
@@ -138,4 +135,25 @@ func readOutputFile(content string) (map[string]string, error) {
 		value.WriteString(line)
 	}
 	return m, nil
+}
+
+func TestParseGitHubOutputFile(t *testing.T) {
+	tt := newT(t)
+	tt.act.SetOutput("foo", "bar")
+	tt.act.SetOutput("baz", "qux")
+	out, err := tt.outputs()
+	require.NoError(t, err)
+	require.EqualValues(t, map[string]string{
+		"foo": "bar",
+		"baz": "qux",
+	}, out)
+}
+
+func TestSetInput(t *testing.T) {
+	tt := newT(t)
+	tt.setInput("hello-world", "greetings")
+	tt.setInput("goodbye-friends", "farewell")
+
+	require.Equal(t, "greetings", tt.act.GetInput("hello-world"))
+	require.Equal(t, "farewell", tt.act.GetInput("goodbye-friends"))
 }
