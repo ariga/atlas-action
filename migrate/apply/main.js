7125ca1000eedd936ea8a5277092b5add5e56d4c
Rotem Tamir <rotemtamir@gmail.com>
2023-09-24 10:06:28 +0300

    verbose

--- migrate/apply/main.js
+++ migrate/apply/main.js
@@ -1,6 +1,20 @@
-const childProcess = require('child_process'); // Import the child_process module
+const childProcess = require('child_process');
+
+// Log all environment variables
+console.log('Environment Variables:');
+for (const [key, value] of Object.entries(process.env)) {
+    console.log(`${key}: ${value}`);
+}
 
 const mainCommand = 'atlas-action';
-const args = ['--action', 'migrate/apply']; // You can put the arguments in an array
+const args = ['--action', 'migrate/apply'];
 
 const spawnSyncReturns = childProcess.spawnSync(mainCommand, args, { stdio: 'inherit' });
+
+// Get the exit code
+const exitCode = spawnSyncReturns.status;
+
+// Or if this is a script, you can set the process exit code
+if (exitCode !== 0) {
+    process.exit(exitCode);
+}
