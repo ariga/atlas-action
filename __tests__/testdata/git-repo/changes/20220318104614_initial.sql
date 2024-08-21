2edbaa069325d3db9987ca302078618209fc5906
Giau. Tran Minh <hello@giautm.dev>
2024-08-21 15:02:54 +0700

    chore: remove deprecated action source code

--- __tests__/testdata/git-repo/changes/20220318104614_initial.sql
+++
@@ -1,3 +0,0 @@
--- create "tbl" table
-CREATE TABLE tbl (`col` int NOT NULL);
-CREATE TABLE tbl2 (`col` int NOT NULL);
--- __tests__/testdata/sqlite-with-diagnostics/20220823074933_add_name.sql
+++
@@ -1,2 +0,0 @@
--- add column "name" to table: "users"
-ALTER TABLE `users` ADD COLUMN `name` varchar NOT NULL;
