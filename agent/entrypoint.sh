989f9bbd23a1eb4fd4bedc236a0e67a45855ab6b
Ronen Lubin <ronen7822@gmail.com>
2024-11-07 13:41:50 +0200

    make entrypoint.sh exec

--- agent/entrypoint.sh
+++ agent/entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh -l
 
-./atlas-agent --token "$CLOUD_TOKEN" &
+echo "Running Atlas Agent"
+# print all the files in the current directory
+# Debugging: Print current directory and list files
+echo "Current Directory: $(pwd)"
+ls -l
+
+/atlas-agent --token "$CLOUD_TOKEN" &
 sleep "$WAIT_TIME"
