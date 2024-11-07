b23e4b1a462c29285ebef63069f1d482a12b5779
Ronen Lubin <ronen7822@gmail.com>
2024-11-07 15:20:16 +0200

    fix dockerfile

--- agent/entrypoint.sh
+++ agent/entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/sh -l
 
-echo "Running Atlas Agent"
-# print all the files in the current directory
-# Debugging: Print current directory and list files
-echo "Current Directory: $(pwd)"
-ls -l
+set -e
 
+echo "Running Atlas Agent"
 /atlas-agent --token "$CLOUD_TOKEN" &
 sleep "$WAIT_TIME"
