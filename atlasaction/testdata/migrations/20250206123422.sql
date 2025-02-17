da953d365e7a4fe9442de5ba91af0a1ebcce81e7
Noam Cattan <62568565+noamcattan@users.noreply.github.com>
2025-02-17 10:36:48 +0200

    Update atlasaction/testdata/migrations/20250206123422.sql
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- atlasaction/testdata/migrations/20250206123422.sql
+++ atlasaction/testdata/migrations/20250206123422.sql
@@ -1,1 +1,8 @@
+-- atlas:txtar
+
+-- checks/destructive.sql --
+-- atlas:assert DS103
+SELECT NOT EXISTS (SELECT 1 FROM `t1` WHERE `c2` IS NOT NULL) AS `is_empty`;
+
+-- migration.sql --
 alter table t1 drop column c2;
