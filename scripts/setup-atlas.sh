f10c5b6bfb23ab6de76010759d0666c7d217d7b1
Giau. Tran Minh <hello@giautm.dev>
2026-01-27 01:51:46 +0700

    scripts/shim: added shell version

--- scripts/setup-atlas.sh
+++ scripts/setup-atlas.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env sh
+#!/bin/sh
 # Copyright 2021-present The Atlas Authors. All rights reserved.
 # This source code is licensed under the Apache 2.0 license found
 # in the LICENSE file in the root directory of this source tree.
 
-set -e
+set -eu
 curl -sSf https://atlasgo.sh | sh
-if [ -n "$ATLAS_TOKEN" ]; then
-  atlas login --token $ATLAS_TOKEN
+if [ -n "${ATLAS_TOKEN:-}" ]; then
+  atlas login --token "$ATLAS_TOKEN"
 fi
