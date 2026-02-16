27e60720912b3da8fa37d8b103bf5a88f0b24173
Giau. Tran Minh <12751435+giautm@users.noreply.github.com>
2026-02-16 22:12:53 +0700

    scripts/setup-atlas: only install atlas-cli if not exists (#473)
    Some CI/CD platforms (like TeamCity), allow run its step in specified
    container image (which can be arigaio/atlas image). So if atlas-cli
    already exists on the system, install it for every step call is
    unnecessary.

--- scripts/setup-atlas.sh
+++ scripts/setup-atlas.sh
@@ -4,7 +4,12 @@
 # in the LICENSE file in the root directory of this source tree.
 
 set -eu
-curl -sSf https://atlasgo.sh | sh
+if command -v atlas > /dev/null 2>&1; then
+  echo "Found pre-installed atlas-cli"
+  atlas version
+else
+  curl -sSf https://atlasgo.sh | sh
+fi
 if [ -n "${ATLAS_TOKEN:-}" ]; then
   atlas login --token "$ATLAS_TOKEN"
 fi
